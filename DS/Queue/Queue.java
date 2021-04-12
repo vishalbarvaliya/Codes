704ebe1d56c43fa1f3505eee4e1b3d2c1e934564
Vishal Barvaliya <40349160+vishalbarvaliya@users.noreply.github.com>
2021-04-12 18:45:41 +0530

    Update Queue.java

--- DS/Queue/Queue.java
+++ DS/Queue/Queue.java
@@ -1,68 +1,81 @@
 class Runners {
+
     public static void main(String[] args) {
         Queue que = new Queue();
         que.enQueue(10);
-        //que.enQueue(20);
-        que.deQueue();            
-        
+        que.enQueue(20);
+        //que.enQueue(30);
+        que.deQueue();
+
         System.out.println(que.isEmpty());
         que.display();
     }
 }
+
 class Queue {
+
     int[] queue = new int[5];
-    int front = 0;
-    int rear = 0;
-    int size = 0;
-    
+    int front = -1;
+    int rear = -1;
+
     public void enQueue(int data) {
-        
-        if(isFull()){
+
+        if(isFull()) {
             System.out.println("Could not enqueue data, Queue is overflowed!");
-        } else {
+        }
+        else if(isEmpty()){
+            front = rear = 0;
             queue[rear] = data;
+        }
+        else{
             rear = (rear + 1) % 5;
-            size++;
+            queue[rear] = data;
         }
     }
-    
+
     public int deQueue() {
         int data = 0;
-        if(isEmpty())
+        if (isEmpty()) {
             System.out.println("Could not dequeue data, Queue is empty!");
+        } 
+        else if (front == rear){
+            data = queue[front];
+            front = rear = -1;
+        }            
         else {
             data = queue[front];
-            front = (front + 1) % 5;
-            size--;
+            front = (front + 1) % 5;            
         }
         return data;
     }
-    
+
     public void display() {
-        if(isEmpty())
+        if (isEmpty()) {
             System.out.println("Could not retrieve data, Queue is empty!");
-        else {
-            for(int i = 0; i < size; i++){
-                System.out.print(queue[(front + i) % 5] + " ");
+        } else {
+            int i = front;
+            while(i != rear) {
+                System.out.print(queue[i] + " ");
+                i = (i + 1) % queue.length;
             }
+            System.out.print(queue[rear]);
         }
     }
-    
-    public int peek(){
-        if(isEmpty()){
+
+    public int peek() {
+        if (isEmpty()) {
             System.out.println("Could not retrieve peek element, Queue is empty!");
             return 0;
-        }            
-        else
+        } else {
             return queue[front];
+        }
     }
-    
+
     public boolean isFull() {
-        return size == queue.length;
+        return (rear + 1) % queue.length == front;
     }
-    
+
     public boolean isEmpty() {
-        return size == 0;
+        return front == -1 && rear == -1;
     }
 }
-
