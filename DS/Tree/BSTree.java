d7267d8369f78dd5cec8ae8ac240631377d67a1e
Vishal Barvaliya <40349160+vishalbarvaliya@users.noreply.github.com>
2021-05-08 11:59:18 +0530

    Moderate

---
+++ DS/Tree/BSTree.java
@@ -0,0 +1,143 @@
+package dsa;
+
+import java.util.Queue;
+
+class TNode {
+    int data;
+    TNode left;
+    TNode right;
+    
+    public TNode(int data){
+        this.data = data;
+    }
+}
+class BSTree {
+    TNode root;
+    
+    public static void main (String[] args) {
+        BSTree tree = new BSTree();
+        tree.add(6);
+        tree.add(4);
+        tree.add(8);
+        tree.add(3);
+        tree.add(5);
+        tree.add(7);
+        tree.add(9);
+        tree.inOrderTraversal(tree.root);
+        //System.out.println(tree.containsTNode(5));
+        tree.levelOrderTraversalBFS(tree.root);        
+    }
+    
+    public void add(int data){
+        root = addRecursive(root, data);
+    }
+    
+    public boolean containsTNode(int data) {
+        return searchRecursive(root, data);
+    }
+    
+    public void remove(int data) {
+        root = deleleTNode(root, data);
+    }
+    
+    public void inOrderTraversal(TNode current) {
+        if(current != null) {
+            inOrderTraversal(current.left);
+            System.out.print(current.data + " ");
+            inOrderTraversal(current.right);
+        }
+    }
+    
+    public void preOrderTraversal(TNode current) {
+        if(current != null) {
+            System.out.print(current.data + " ");
+            preOrderTraversal(current.left);
+            preOrderTraversal(current.right);
+        }
+    }
+    
+    public void postOrderTraversal(TNode current) {
+        if(current !=  null) {
+            postOrderTraversal(current.left);
+            postOrderTraversal(current.right);
+            System.out.print(current.data + " ");
+        }
+    }
+    
+    public void levelOrderTraversalBFS(TNode current) {
+        Queue<TNode> nodes = new java.util.LinkedList<>();
+        nodes.add(current);
+        
+        while(!(nodes.isEmpty())) {
+            TNode temp = nodes.remove();
+            System.out.print(temp.data + " ");
+            
+            if(temp.left != null)
+                nodes.add(temp.left);
+            if(temp.right != null)
+                nodes.add(temp.right);
+        }
+    }
+    
+    private TNode addRecursive(TNode current, int data) {
+        if (current == null) {
+            return new TNode(data);
+        } 
+        else {
+            if(data < current.data) {
+                current.left = addRecursive(current.left, data);
+            }
+            else if(data > current.data) {
+                current.right = addRecursive(current.right, data);
+            } 
+            else {
+                return current;
+            }            
+        }
+        return current;
+    }   
+    
+    private boolean searchRecursive(TNode current, int data) {
+        if(current == null) {
+            return false;
+        }
+        if(current.data == data){
+            return true;
+        }
+        return data < current.data
+            ? searchRecursive(current.left, data)
+            : searchRecursive(current.right, data);
+    }
+    
+    private TNode smallestTNode(TNode temp) {
+        if(temp.left == null)
+            return temp;
+        else 
+            return smallestTNode(temp.left);
+    }
+    
+    private TNode deleleTNode(TNode current, int data) {
+        if(current == null)
+            return null;
+        if (data < current.data)
+            current.left = deleleTNode(current.left, data);
+        else if(data > current.data) 
+            current.left = deleleTNode(current.right, data);
+        else {
+            
+            if(current.left != null && current.right != null) {
+                TNode temp = root;
+                TNode minTNode = smallestTNode(temp.right);
+                current.data = minTNode.data;
+                current.right = deleleTNode(current.right, minTNode.data);
+            }
+            else if(current.left != null)
+                current = current.left;
+            else if(current.right != null)
+                current = current.right;
+            else 
+                current = null;
+        }
+        return current;
+    }    
+}
